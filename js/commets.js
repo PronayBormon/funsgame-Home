d41b0c0c1551cfbc1f424c98f14f9f6e07f10f6f
PronayBormon <pronaykumarbormon@gmail.com>
2023-06-25 19:26:06 +0600

    add mobile comment section

---
+++ js/commets.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var x = document.getElementById("comments");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.opacity = "1";
+    }
+  }
+
+  jQuery(document).ready(function($) {
+    // auto timer
+    setTimeout(function() {
+      $('#lab-slide-bottom-popup').modal('show');
+    }, 5000); // optional - automatically opens in xxxx milliseconds
+  
+    $(document).ready(function() {
+      $('.lab-slide-up').find('a').attr('data-toggle', 'modal');
+      $('.lab-slide-up').find('a').attr('data-target', '#lab-slide-bottom-popup');
+    });
+  
+  });
