f137b19f1e922e5bae7af5b6398dcc2da334eaad
PronayBormon <pronaykumarbormon@gmail.com>
2023-07-22 22:15:19 +0600

    active pause option and adjust height

--- js/scrollToPlayVideo.js
+++ js/scrollToPlayVideo.js
@@ -1,34 +1,54 @@
-const videoContainer = document.querySelector('.video-container');
-const videoitems = document.querySelectorAll('.video-item');
+// const videoContainer = document.querySelector('.video-container');
+// const videoitems = document.querySelectorAll('.video-item');
 
-// Autoplay videos when they enter the viewport
-function autoplayVideo() {
+// // Autoplay videos when they enter the viewport
+// function autoplayVideo() {
   
-  videoitems.forEach((video) => {
-    const rect = video.getBoundingClientRect();
-    const isVisible = rect.top >= 1 && rect.bottom <= window.innerHeight;
+//   videoitems.forEach((video) => {
+//     const rect = video.getBoundingClientRect();
+//     const isVisible = rect.top >= 1 && rect.bottom <= window.innerHeight;
 
-    if (isVisible && video.paused) {
-      video.play();
-      video.mute= false; // Enable sound
-    } else if (!isVisible && !video.paused) {
-      video.pause();
-      video.mute = true; // Disable sound
+//     if (isVisible && video.paused) {
+//       video.play();
+//       video.mute= false; // Enable sound
+//     } else if (!isVisible && !video.paused) {
+//       video.pause();
+//       video.mute = true; // Disable sound
+//     }
+//   });
+// }
+// // Initial autoplay on page load
+// autoplayVideo();
+
+// // Event listener for scrolling
+// videoContainer.addEventListener('scroll', autoplayVideo);
+
+
+
+
+// // click to play video 
+
+// $('video').click(function(){
+// 	// Toggle Magic Here
+//     this[this.paused ? 'play' : 'pause']();
+// });
+const videos = document.querySelectorAll('.video-container .video-wrapper video');
+
+for(let i= 0; i< videos.length; i++){
+  videos[i]. addEventListener("mouseenter", () => {
+    videos[i].currentTime =0;
+    videos[i].loop = true;
+    videos[i].play();
+    videos[i].mute = true;
+  });
+  videos[i].addEventListener("mouseleave", () => {
+    videos[i].pause();
+  });
+  videos[i].addEventListener("click", () => {
+    if(videos[i].paused){
+      videos[i].play();
+    } else{
+      videos[i].pause();
     }
   });
 }
-// Initial autoplay on page load
-autoplayVideo();
-
-// Event listener for scrolling
-videoContainer.addEventListener('scroll', autoplayVideo);
-
-
-
-
-// click to play video 
-
-$('video').click(function(){
-	// Toggle Magic Here
-    this[this.paused ? 'play' : 'pause']();
-});
