d1e1682eb4c808503e3233164ecd34ebab5ad7af
PronayBormon <pronaykumarbormon@gmail.com>
2023-09-25 13:38:49 +0600

    preloader test

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,37 @@
-var loader = document.getElementById("preloader");
+// var loader = document.getElementById("preloader");
 
-        window.addEventListener("load", function(){
-            loader.style.display = "none";
-        })
+//         window.addEventListener("load", function(){
+//             loader.style.display = "none";
+//         })
+
+// $(document).ready(function(){
+    // $(window).load(function(){
+    
+    // });
+// });
+
+// $(window).load(function() {
+//     $('.preloader-wrapper').fadeOut();
+//     $('.loaders').addClass('preloader-site');
+// });
+
+// $(document).ready(function() {
+//     // executes when HTML-Document is loaded and DOM is ready
+//     alert("document is ready");
+    
+//     // $("loaders").removeClass("loaders");
+//    });
+   
+   
+//    $(window).load(function() {   
+//    });
+
+
+$(document).ready(function($) {
+    const Body = $('.loaders');
+    Body.addClass('loaded');
+});
+// $(window).load(function() {
+//     $('loaders').fadeOut();
+//     $('loaders').addClass('loaded');
+// });
